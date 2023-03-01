f6b8bc8f3d478b6def3da9f1c6378c502efda363
Renju <xyz@gmail.com>
2023-03-02 00:14:10 +0530

    updated

--- Orangehrm1/src/pack1/Login.java
+++ Orangehrm1/src/pack1/Login.java
@@ -1,10 +1,15 @@
 package pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.setProperty("webdriver.gecko.driver","C:\\selenium projects\\geckodriver-w0.29.0-win64\\geckodriver.exe");
+WebDriver driver=new FirefoxDriver();
+driver.get("https://localhost/orangehrm1/web/index.php/auth/login");
 	}
 
 }
