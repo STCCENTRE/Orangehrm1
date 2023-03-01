b95d147a76dd523e1e451712e56e47a680eea82c
anitap <xyz@gmail.com>
2023-03-01 10:37:07 +0530

    added new statements

--- Orangehrm1/src/pack1/Login.java
+++ Orangehrm1/src/pack1/Login.java
@@ -1,10 +1,17 @@
 package pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.gecko.driver","C:\\selenium projects\\geckodriver-v0.29.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		//open URL-ORANGE-HRM
+		driver.get("http://localhost/orangehrm/web/index.php/auth/login");
 	}
 
 }
