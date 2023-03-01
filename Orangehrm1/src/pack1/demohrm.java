1c2a1c581d90c956171272e7a5906758eb728764
kavya <kavyachinnu1099@gmail.com>
2023-03-01 11:31:11 +0530

    add new statement

---
+++ Orangehrm1/src/pack1/demohrm.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class demohrm {
+
+	public static void main(String[]args){
+System .setProperty("webdriver.gecko.driver" ,"C:\\Users\\kavya\\Downloads\\geckodriver-v0.29.1-win32\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		//open URL-ORANGE-HRM
+} 
