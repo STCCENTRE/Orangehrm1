1f788727ece5f41580727ac97365fa7a91415636
sandra <sandramurali666@gmail.com>
2023-03-02 11:43:57 +0530

    new statement added

--- Orangehrm1/src/pack1/Login.java
+++ Orangehrm1/src/pack1/Login.java
@@ -1,10 +1,18 @@
 package pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver","C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		//open URL-ORANGE-HRM
+		driver.get("http://localhost/orangehrm/web/index.php/auth/login");
 	}
 
 }
